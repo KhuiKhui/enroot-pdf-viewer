89ff676a3494ef4e1089ce13838a7368b1539eab
KhuiKhui <khoikhuat0807@gmail.com>
2026-01-09 14:11:47 +0800

    fix: fix reactpdf error
    https://github.com/diegomura/react-pdf/issues/2599

--- src/components/PDFPreview/PDFPreview.tsx
+++ src/components/PDFPreview/PDFPreview.tsx
@@ -1,7 +1,16 @@
 'use client';
 
+import dynamic from 'next/dynamic';
+
+const PDFViewer = dynamic(
+  () => import('@react-pdf/renderer').then((mod) => mod.PDFViewer),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  },
+);
+
 import PDFTemplate from '../PDFTemplate/PDFTemplate';
-import { PDFViewer } from '@react-pdf/renderer';
 function PDFPreview() {
   return (
     <div className="size-full rounded-md bg-gray-300 p-4">
