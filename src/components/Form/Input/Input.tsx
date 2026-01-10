d15d6805aa5f6657c93cf91c7f3c20a8157a58ba
Leo Vo <vokhangtom@gmail.como>
2026-01-10 21:56:06 +0800

    Imported fonts and added styling to body and title text

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -1,7 +1,7 @@
 import cn from '@/utils/cn';
 
 //const baseInputStyles = 'border-b-2 border-gray-500 bg-purple-100 p-4 focus:border-purple-700 focus:outline-hidden';
-const baseStyles = "bg-[#FFD8E4] border-b-2 border-[#FFD8E4] p-4 focus:bg-[#FFF2F6] focus:outline-hidden";
+const baseStyles = `bg-[var(--color-base)] border-b-2 border-[var(--color-base)] p-4 focus:bg-[var(--color-focus)] focus:outline-hidden`;
 interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
   label?: string;
 }
@@ -16,7 +16,7 @@ function Input({ label = 'Placeholder', className, ...base }: InputProps) {
           className,
         )}
       />
-      <div className="text-sm text-gray-500">{label}</div>
+      <div className="text-sm text-gray-500 font-[var(--font-text)]">{label}</div>
     </div>
   );
 }
