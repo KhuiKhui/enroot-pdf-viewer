5dfa5039a1faef388b51dedf93d502035fe9db7a
Leo Vo <vokhangtom@gmail.como>
2026-01-10 20:28:42 +0800

    Added more styling to input fields

--- src/components/Form/Input/Input.tsx
+++ src/components/Form/Input/Input.tsx
@@ -1,16 +1,18 @@
 import cn from '@/utils/cn';
 
+//const baseInputStyles = 'border-b-2 border-gray-500 bg-purple-100 p-4 focus:border-purple-700 focus:outline-hidden';
+const baseStyles = "bg-[#FFD8E4] border-b-2 border-[#FFD8E4] p-4 focus:bg-[#FFF2F6] focus:outline-hidden";
 interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
   label?: string;
 }
-
 function Input({ label = 'Placeholder', className, ...base }: InputProps) {
   return (
     <div className="flex flex-col justify-center gap-2">
       <input
         {...base}
         className={cn(
-          'border-b-2 border-gray-500 bg-purple-100 p-4 focus:border-purple-700 focus:outline-hidden',
+          //baseInputStyles,
+          baseStyles,
           className,
         )}
       />
---
+++ src/styles.ts
@@ -0,0 +1,6 @@
+//Not sure if the web is complicated enough to need centralised styles yet
+//const colors = {
+//  base: '#FFD8E4', //color for input background and border
+//  focus: '#FFF2F6', //color for input background on focus
+//};
+
